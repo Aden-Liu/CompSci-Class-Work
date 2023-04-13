d876e99cbce90a939b8bdb4f29133623555ac649
Aden-Liu <adenliu20@gmail.com>
2023-04-13 01:19:40 -0400

    removed unneeded get request

--- script.js
+++ script.js
@@ -19,21 +19,6 @@ function getDetails() {
     });
 }
 
-function getTrailer() {
-  axios({
-    method: "get",
-    url: `https://api.themoviedb.org/3/movie/${
-      choices[document.getElementById("movies").selectedIndex]
-    }/videos?api_key=186585b7b6aff9c8ea3b6e64e18c88f0&language=en-US`,
-  })
-    .then((response) => {
-      console.log(response);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-}
-
 function createMovieTile(result) {
   let title = document.createElement("h1");
   let poster = document.createElement("img");
