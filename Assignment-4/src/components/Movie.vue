9bf5c8c10ac1c73b5fe899f2a4a9b81c429be2fb
unknown <342221900@TDSB.ON.CA>
2023-05-11 15:14:57 -0400

    bruh

--- Assignment-4/src/components/Movie.vue
+++ Assignment-4/src/components/Movie.vue
@@ -1,93 +1,49 @@
-<script setup>
+<script>
 import { apiKey } from "./key.js";
 import axios from "axios";
-import { ref } from 'vue';
+import { ref } from "vue";
 
-function getMovieID() {
-  let movieID = parseInt(document.getElementById("movies").value);
-  return movieID;
-}
+// function getMovieID() {
+//   let movieID = parseInt(document.getElementById("movies").value);
+//   return movieID;
+// }
 
-async function getDetails() {
-  await axios({
-    method: "get",
-    url: `https://api.themoviedb.org/3/movie/${getMovieID()}?api_key=${apiKey}&language=en-US&append_to_response=videos`,
-  })
-    .then((result) => {
-      console.log(result);
-      createMovieTile(result);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-}
+// async function getDetails() {
+//   await axios({
+//     method: "get",
+//     url: `https://api.themoviedb.org/3/movie/${getMovieID()}?api_key=${apiKey}&language=en-US&append_to_response=videos`,
+//   })
+//     .then((result) => {
+//       console.log(result);
+//       createMovieTile(result);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
 
-function createMovieTile(result) {
-  document.getElementById("movie").innerHTML = "";
-  let page = ref("tile");
-  let title = document.createElement("h1");
-  let poster = document.createElement("img");
-  let date = document.createElement("h3");
-  let synopsis = document.createElement("p");
-  let budget = document.createElement("h3");
-  let runtime = document.createElement("h3");
-  let popularity = document.createElement("h3");
-  let revenue = document.createElement("h3");
-  let tagline = document.createElement("p");
-  let voteAverage = document.createElement("h3");
-  let voteCount = document.createElement("h3");
-  let trailer = document.createElement("iframe");
+export default {
+  setup() {
+    let movieSelect = ref("75612");
+    let movieData = ref(null);
 
-  title.innerHTML = `${result.data.title}`;
-  title.setAttribute("class", "details-box");
-  title.setAttribute("id", "title-box");
-  poster.src = `https://image.tmdb.org/t/p/w500${result.data.poster_path}`;
-  poster.setAttribute("id", "movie-poster");
-  date.innerHTML = `Released: ${result.data.release_date}`;
-  date.setAttribute("class", "info-box");
-  synopsis.innerHTML = `${result.data.overview}`;
-  synopsis.setAttribute("class", "info-box");
-  budget.innerHTML = `Budget: $${result.data.budget}`;
-  budget.setAttribute("class", "info-box");
-  revenue.innerHTML = `Revenue: $${result.data.revenue}`;
-  revenue.setAttribute("class", "info-box");
-  runtime.innerHTML = `Runtime: ${result.data.runtime}  Minutes`;
-  runtime.setAttribute("class", "info-box");
-  popularity.innerHTML = `Popularity: ${result.data.popularity}`;
-  popularity.setAttribute("class", "info-box");
-  tagline.innerHTML = `"${result.data.tagline}"`;
-  tagline.setAttribute("class", "info-box");
-  voteAverage.innerHTML = `Rating: ${result.data.vote_average}/10`;
-  voteAverage.setAttribute("class", "info-box");
-  voteCount.innerHTML = `# of Ratings: ${result.data.vote_count}`;
-  voteCount.setAttribute("class", "info-box");
+    const getDetails = async () => {
+      const result = await axios.get(`https://api.themoviedb.org/3/movie/${movieSelect.value}?api_key=${apiKey}&language=en-US&append_to_response=videos`);
+      movieData.value = result.data;
+    }
 
-  let trailerData = result.data.videos.results.filter((trailer) => {
-    return trailer.type === "Trailer";
-  });
-
-  trailer.src = `https://www.youtube.com/embed/${trailerData.at(0).key}`;
-  trailer.setAttribute("id", "trailer-box");
-
-  page.appendChild(poster);
-  page.appendChild(title);
-  page.appendChild(date);
-  page.appendChild(synopsis);
-  page.appendChild(tagline);
-  page.appendChild(budget);
-  page.appendChild(runtime);
-  page.appendChild(popularity);
-  page.appendChild(revenue);
-  page.appendChild(voteAverage);
-  page.appendChild(voteCount);
-  page.appendChild(trailer);
+    return {
+      movieSelect,
+      movieData,
+      getDetails
+    }
+  },
 }
 </script>
 
 <template>
   <form id="movie-select">
     <label for="movies">Choose a movie</label>
-    <select name="movies" id="movies">
+    <select v-model="movieSelect" id="movies">
       <option value="75612">Oblivion</option>
       <option value="157336">Interstellar</option>
       <option value="406759">Moonfall</option>
@@ -99,9 +55,10 @@ function createMovieTile(result) {
       <option value="412656">Chaos Walking</option>
       <option value="419704">Ad Astra</option>
     </select>
-    <button id="get-button" type="button" @click="getDetails()">Get</button>
+    <button id="get-button" type="button" @click="getDetails">Get</button>
   </form>
-  <div id="movie" ref="tile"></div>
+  <div id="movie" ref="tile">
+  </div>
 </template>
 
 <style scoped></style>
--- my-vue-app/src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-  import Movie from './components/Movie.vue';
-</script>
-
-<template>
-  <Movie>bruh</Movie>
-</template>
-
-<style scoped></style>
