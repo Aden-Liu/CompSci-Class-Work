2c79b3f17dccf14365815b794197fd549165deea
unknown <342221900@TDSB.ON.CA>
2023-05-23 14:31:09 -0400

    added outline files

---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { useRouter } from "vue-router";
+</script>
+
+<template>
+  <div id="header-container">
+    <h1>WASD Inc.</h1>
+    <button>Login</button>
+  </div>
+</template>
+
+<style scoped></style>
