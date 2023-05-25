0e819d56444b6455bdeb7b18d4a320b97190c63f
Liu <Aden.Liu@student.tdsb.on.ca>
2023-05-25 12:54:29 -0400

    fixed posters

--- src/main.js
+++ src/main.js
@@ -4,6 +4,6 @@ import './style.css'
 import App from './App.vue'
 import { router } from "./router";
 
-const store = createPinia();
+const pinia = createPinia();
 
-createApp(App).use(router).use(store).mount('#app')
+createApp(App).use(router).use(pinia).mount('#app')
--- src/pages/PurchasePage.vue
+++ src/pages/PurchasePage.vue
@@ -1,26 +1,30 @@
 <script setup>
 import axios from "axios";
 
-const movieData = await axios.get(
-  "https://api.themoviedb.org/3/movie/popular",
-  {
+const movieData = (
+  await axios.get("https://api.themoviedb.org/3/movie/popular", {
     params: {
       api_key: import.meta.env.VITE_TMDB_API_KEY,
+      region: "US",
+      language: "en",
       include_adult: false,
     },
-  }
-);
+  })
+).data;
 
 console.log(movieData);
 </script>
 
 <template>
   <div v-if="movieData" class="tiles">
-    <div v-for="i in movieData.data.results">
+    <div v-for="movie in movieData.results" class="info-container">
+    <h1>{{ movie.title }}</h1>
       <img
-        :src="`https://image.tmdb.org/t/p/w500${movieData.data.poster_path}`"
+        class="movie-poster"
+        :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`"
         alt="movie poster"
       />
+      <h3>{{ movie.release_date }}</h3>
     </div>
   </div>
 </template>
