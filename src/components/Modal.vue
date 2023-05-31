29031ee7dd99dae63550b273c601f940102797c3
Liu <Aden.Liu@student.tdsb.on.ca>
2023-05-31 14:10:50 -0400

    added modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,18 +1,31 @@
 <script setup>
 import axios from "axios";
 
-// const store = useStore();
+const store = useStore();
 const props = defineProps(["id"]);
 
-let open = false;
+const movieData = (
+  await axios.get("https://api.themoviedb.org/3/movie/popular", {
+    params: {
+      api_key: import.meta.env.VITE_TMDB_API_KEY,
+      region: "US",
+      language: "en",
+      include_adult: false,
+    },
+  })
+).data;
 </script>
 
 <template>
-  <button type="button" @click="open = true">Open Modal</button>
   <Teleport to="body">
-    <div class="modal" v-if="open == true">
-      <p>Modal test</p>
-      <button type="button" @click="open = false">Close Modal</button>
+    <div class="modal-container">
+      <button type="button" @click="$emit('showModal')">X</button>
+      <div v-if="movieData">
+      <img :src="`https://image.tmdb.org/t/p/w500/${movieData.poster_path}`" alt="Movie Poster">
+      <h1>{{ movieData.title }}</h1>
+      <h2>{{ movieData.release_date }}</h2>
+      <button type="button" @click="store.addToCart(movieData.poster_path, movieData.title)">Add to Cart</button>
+      </div>
     </div>
   </Teleport>
 </template>
--- src/pages/PurchasePage.vue
+++ src/pages/PurchasePage.vue
@@ -6,9 +6,8 @@ import { useStore } from "../store";
 
 const router = useRouter();
 const store = useStore();
-const modalView = ref(false);
-const selectedID = ref(null);
-
+let modalView = ref(false);
+let selectedID = ref(null);
 
 const movieData = (
   await axios.get("https://api.themoviedb.org/3/movie/popular", {
@@ -21,22 +20,27 @@ const movieData = (
   })
 ).data;
 
-const showModal = () => {
-  show
-}
+const showModal = (movieID) => {
+  modalView = !modalView;
+  selectedID.value = movieID;
+};
 </script>
 
 <template>
-  <div v-if="movieData" class="tiles">
-    <div v-for="movie in movieData.results" class="info-container">
-      <img
-        class="movie-poster"
-        :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`"
-        alt="movie poster"
-        @click="showModal(movie.id)"
-      />
+  <div>
+    <button type="button" @click="router.push('/cart')">Cart</button>
+    <div v-if="movieData" class="tiles">
+      <div v-for="movie in movieData.results" class="info-container">
+        <img
+          class="movie-poster"
+          :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`"
+          alt="movie poster"
+          @click="showModal(movie.id)"
+        />
+      </div>
     </div>
   </div>
+  <Modal v-if="showModal" :id="selectedID" @showModal="showModal"/>
 </template>
 
 <style scoped></style>
